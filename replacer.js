11896ba6d03c847b7426fc4e21224d7f27be763a
whiteSkar <sypower07@Hotmail.com>
2024-03-15 15:17:00 -0700

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -1,9 +1,8 @@
-document.addEventListener('copy', function(event) {
-  var clipboardData = event.clipboardData || window.clipboardData;
-  var copiedText = clipboardData.getData('text');
-  
-  if (copiedText.includes("slack.com/archives")) {
-    var modifiedText = copiedText.replace("archives", "messages");
-    clipboardData.setData('ext', modifiedText);
-  }
+chrome.clipboard.onClipboardDataChanged.addListener(function() {
+  chrome.clipboard.readText(function(copiedText) {
+    if (copiedText.includes("slack.com/archives")) {
+      var modifiedText = copiedText.replace("archives", "messages");
+      chrome.clipboard.writeText(modifiedText);
+    }
+  });
 });
