4b2bfa61ee7df1ca8361f6fd4ff6f376e8543467
whiteSkar <sypower07@Hotmail.com>
2024-03-15 15:05:48 -0700

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -1,19 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      function: replaceArchivesWithMessages,
-    });
-  });
+document.addEventListener('copy', function(event) {
+  var clipboardData = event.clipboardData || window.clipboardData;
+  var copiedText = clipboardData.getData('text');
+  
+  if (copiedText.includes("slack.com/archives")) {
+    var modifiedText = copiedText.replace("archives", "messages");
+    clipboardData.setData('ext', modifiedText);
+    
+    event.preventDefault();
+  }
 });
-
-function replaceArchivesWithMessages() {
-  navigator.clipboard.readText().then((text) => {
-    if (text.includes("slack.com/archives")) {
-      const newText = text.replace("archives", "messages");
-      navigator.clipboard.writeText(newText).then(() => {
-        console.log('Clipboard content replaced from slack.com/archives to slack.com/messages');
-      });
-    }
-  });
-}
