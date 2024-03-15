92a716919d14ec2b5eadc0094cffb83e90c3a8f9
whiteSkar <sypower07@Hotmail.com>
2024-03-15 15:54:54 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+try {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.event == "copy") {
       alert("copy detected");
     }
     sendResponse({});
-  }
-);
+  });
+} catch (error) {
+  console.error("Error adding message listener:", error);
+}
