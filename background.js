a615bb90f736d67684540a8f315ff9c47028be80
whiteSkar <sypower07@Hotmail.com>
2024-03-15 16:11:43 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.warn("whiteSkar - message Received", message.event)
   // Check if the message indicates a clipboard copy event
   if (message.event === 'clipboard-copy') {
     // Perform actions in response to the clipboard copy event
-    console.log('Clipboard content copied!');
+    console.warn('Clipboard content copied!');
     // You can perform further actions here, such as modifying the clipboard content
   }
 });
