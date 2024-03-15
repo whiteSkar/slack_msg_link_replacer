d102eb3545f252d75cee185cdb59d27b0b36e855
whiteSkar <sypower07@Hotmail.com>
2024-03-15 15:07:06 -0700

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -5,7 +5,5 @@ document.addEventListener('copy', function(event) {
   if (copiedText.includes("slack.com/archives")) {
     var modifiedText = copiedText.replace("archives", "messages");
     clipboardData.setData('ext', modifiedText);
-    
-    event.preventDefault();
   }
 });
