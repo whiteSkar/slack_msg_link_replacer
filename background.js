cc7b480f15d605ab719a2d2848830608fd6f91ff
whiteSkar <sypower07@Hotmail.com>
2024-03-23 03:00:56 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-// Listen for clicks on the extension button
-chrome.action.onClicked.addListener(function(tab) {
-  // Send a message to the content script to invoke the replaceTextInEditableElements function
-  chrome.tabs.sendMessage(tab.id, { action: 'replaceClipboard' });
-  // chrome.tabs.sendMessage(tab.id, { action: 'replaceText' });
-});
+// // Listen for clicks on the extension button
+// chrome.action.onClicked.addListener(function(tab) {
+//   // Send a message to the content script to invoke the replaceTextInEditableElements function
+//   chrome.tabs.sendMessage(tab.id, { action: 'replaceClipboard' });
+//   // chrome.tabs.sendMessage(tab.id, { action: 'replaceText' });
+// });
