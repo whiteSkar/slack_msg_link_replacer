0761cf38be1b8927992a79a214ea895cf5b0c241
whiteSkar <sypower07@Hotmail.com>
2024-03-23 02:52:58 -0700

    Update copy_listener.js

--- copy_listener.js
+++ copy_listener.js
@@ -6,26 +6,37 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   }
 });
 
-// document.addEventListener('copy', async () => {
-//    try {
-//      var input = document.createElement('input');
+document.addEventListener('copy', async () => {
+  var input = document.createElement('input');
+
+  // Position the input off-screen
+  input.style.position = 'absolute';
+  input.style.left = '10px';
   
-//      // Position the input off-screen
-//      input.style.position = 'absolute';
-//      input.style.left = '10px';
-     
-//      // Append the input to the document body
-//      document.body.appendChild(input);
-//      // Focus the input element to ensure the document.execCommand('paste') works
-//      input.focus();
-       
-//      const text = await navigator.clipboard.readText()
-//      input.value = text;
-//      console.warn('Text pasted.');
-//    } catch (error) {
-//      console.warn('Failed to read clipboard');
-//    }
-// });
+  // Append the input to the document body
+  document.body.appendChild(input);
+  // Focus the input element to ensure the document.execCommand('paste') works
+  input.focus();
+    
+  let clipboardContent = await navigator.clipboard.readText()
+  
+  // Check if the clipboard content contains the word "archives"
+  if (clipboardContent.includes('slack.com/archives')) {
+    // Modify the clipboard content
+    clipboardContent = clipboardContent.replace('slack.com/archives', 'slack.com/messages');
+    
+    // Set the modified clipboard content back to the input value
+    input.value = clipboardContent;
+    
+    // Select the input content
+    input.select();
+    
+    // Execute the copy command
+    console.warn("whiteSkar - copy about to be executed with contet", clipboardContent);
+    document.execCommand('copy');
+    console.warn("whiteSkar - copy executed");
+  }
+});
                              
 function replaceTextInClipboard() {
   console.warn("whiteSkar - copy event called with event", event);
