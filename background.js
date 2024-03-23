8fb4ca5ed3c6b20f2de1223575a8c966d825e74c
whiteSkar <sypower07@Hotmail.com>
2024-03-22 21:43:46 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,5 @@
 // Listen for clicks on the extension button
 chrome.action.onClicked.addListener(function(tab) {
   // Send a message to the content script to invoke the replaceTextInEditableElements function
-  console.warn("whiteSkar - button Clicked");
   chrome.tabs.sendMessage(tab.id, { action: 'replaceText' });
 });
-// chrome.action.onClicked.addListener((tab) => {
-//   chrome.scripting.executeScript({
-//     target: {tabId: tab.id},
-//     files: ['content.js']
-//   });
-// });
