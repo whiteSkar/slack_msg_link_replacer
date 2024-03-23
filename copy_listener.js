1ff5292c8472a4252ba776889eed3813261b5334
whiteSkar <sypower07@Hotmail.com>
2024-03-23 02:30:09 -0700

    Update copy_listener.js

--- copy_listener.js
+++ copy_listener.js
@@ -1,8 +1,55 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.action === 'replaceText') {
     replaceTextInEditableElements();
+  } else if (message.action === 'replaceClipboard') {
+    replaceTextInClipboard();
   }
 });
+
+function replaceTextInClipboard() {
+  console.log("whiteSkar - copy event called with event", event);
+  // Create a temporary input element
+  var input = document.createElement('input');
+  
+  // Position the input off-screen
+  input.style.position = 'absolute';
+  input.style.left = '-9999px';
+  
+  // Append the input to the document body
+  document.body.appendChild(input);
+
+  // Focus the input element to ensure the document.execCommand('paste') works
+  input.focus();
+
+  console.log("whiteSkar - paste about to be executed");
+  
+  document.execCommand('paste');
+  console.log("whiteSkar - paste executed");
+
+  // Get the clipboard content from the input value
+  var clipboardContent = input.value;
+  console.log("whiteSkar - cliipboard content is", clipboardContent);
+  
+  // Check if the clipboard content contains the word "archives"
+  if (clipboardContent.includes('slack.com/archives')) {
+    // Modify the clipboard content
+    clipboardContent = clipboardContent.replace('slack.com/archives', 'slack.com/messages');
+    
+    // Set the modified clipboard content back to the input value
+    input.value = clipboardContent;
+    
+    // Select the input content
+    input.select();
+    
+    // Execute the copy command
+    console.log("whiteSkar - copy about to be executed with contet", clipboardContent);
+    document.execCommand('copy');
+    console.log("whiteSkar - copy executed");
+  }
+  
+  // Remove the input from the document body
+  document.body.removeChild(input);
+}
 
 // Function to find and replace text in editable elements
 function replaceTextInEditableElements() {
