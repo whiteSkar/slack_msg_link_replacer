3a2b8b6f9f63396be9f38fa227a8b0f35bba9783
whiteSkar <sypower07@Hotmail.com>
2024-03-22 20:01:50 -0700

    Update copy_listener.js

--- copy_listener.js
+++ copy_listener.js
@@ -5,6 +5,7 @@ var copyExecuted = false;
 function handleClipboardEvent(event) {
   // Check if the event type is 'copy'
   if (event.type === 'copy' && !copyExecuted) {
+    console.log("whiteSkar - copy event called with event", event);
     // Create a temporary input element
     var input = document.createElement('input');
     
@@ -17,11 +18,15 @@ function handleClipboardEvent(event) {
 
     // Focus the input element to ensure the document.execCommand('paste') works
     input.focus();
+
+    console.log("whiteSkar - paste about to be executed");
     
     document.execCommand('paste');
+    console.log("whiteSkar - paste executed");
 
     // Get the clipboard content from the input value
     var clipboardContent = input.value;
+    console.log("whiteSkar - cliipboard content is", clipboardContent);
     
     // Check if the clipboard content contains the word "archives"
     if (clipboardContent.includes('archives')) {
@@ -36,7 +41,10 @@ function handleClipboardEvent(event) {
       
       // Execute the copy command
       copyExecuted = true;
+      console.log("whiteSkar - copy about to be executed with contet", clipboardContent);
+      
       document.execCommand('copy');
+      console.log("whiteSkar - copy executed");
     }
     
     // Remove the input from the document body
